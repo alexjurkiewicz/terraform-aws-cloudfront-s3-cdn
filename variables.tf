38bdcde19642a9538b146371129960b4b31a5e15
Alex Jurkiewicz <alex@jurkiewi.cz>
2022-08-29 12:42:30 +1000

    Correct a comment

--- variables.tf
+++ variables.tf
@@ -82,7 +82,7 @@ variable "default_root_object" {
 variable "comment" {
   type        = string
   default     = "Managed by Terraform"
-  description = "Comment for the origin access identity"
+  description = "Comment for the distribution and origin access identity"
 }
 
 variable "log_standard_transition_days" {
